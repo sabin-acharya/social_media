86d9778af227e0e53a4670bb5cb8277fab66f58e
sabin.acharya <sabinacharya9862@gmail.com>
2024-01-25 11:25:13 +0545

    InitialCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/">Text-Upgrade</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarColor01">
+                <ul className="navbar-nav me-auto">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="/">Home
+                      <span className="visually-hidden">(current)</span>
+                    </a>
+                  </li>
+                  
+                  
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">About</a>
+                  </li>
+                 
+                </ul>
+                <form className="d-flex">
+                  <input className="form-control me-sm-2" type="search" placeholder="Search"/>
+                  <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+    </>
   );
 }
 
